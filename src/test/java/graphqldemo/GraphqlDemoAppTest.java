2942720c59930eb177758cd9c70761db8f909838
HuzefaC <HuzefaC>
2022-12-31 10:52:23 +0000

    Initial commit

---
+++ src/test/java/graphqldemo/GraphqlDemoAppTest.java
@@ -0,0 +1,21 @@
+package graphqldemo;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class GraphqlDemoAppTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
